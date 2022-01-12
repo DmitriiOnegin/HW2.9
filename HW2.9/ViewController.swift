f5310cf1594f2eb52c521126425bfe675f63d50b
Dmitrii Onegin <dimimimo@mail.ru>
2022-01-12 19:48:46 +0300

    add outlets

--- HW2.9/ViewController.swift
+++ HW2.9/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var viewLabel: UIView!
+    @IBOutlet weak var presetLabel: UILabel!
+    @IBOutlet weak var curveLabel: UILabel!
+    @IBOutlet weak var forceLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var delayLabel: UILabel!
+    @IBOutlet weak var runButtonLabel: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func runAnimationButton() {
+    }
+    
+    
 }
 
