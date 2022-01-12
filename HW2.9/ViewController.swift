7538d189fcaa88c00851f04c08a62a4589d5e338
Dmitrii Onegin <dimimimo@mail.ru>
2022-01-12 19:58:53 +0300

    change Type view

--- HW2.9/ViewController.swift
+++ HW2.9/ViewController.swift
@@ -5,25 +5,28 @@
 //  Created by Dmitrii Onegin on 12.01.2022.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var viewLabel: UIView!
+   
+    @IBOutlet weak var animationView: SpringView!
     @IBOutlet weak var presetLabel: UILabel!
     @IBOutlet weak var curveLabel: UILabel!
     @IBOutlet weak var forceLabel: UILabel!
     @IBOutlet weak var durationLabel: UILabel!
     @IBOutlet weak var delayLabel: UILabel!
-    @IBOutlet weak var runButtonLabel: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func runAnimationButton() {
+    @IBAction func runAnimationButton(_ sender: SpringButton) {
+        
     }
+    
     
     
 }
